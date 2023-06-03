82ece3e07b2ed9dff29e38002a7a10321edcd46c
ksupipr <ksupipr@yandex.ru>
2023-06-03 23:29:05 +0500

    BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  //<BrowserRouter>
+  <BrowserRouter>
     <App />
-  //</BrowserRouter>
+  </BrowserRouter>
 );
 
 reportWebVitals();
